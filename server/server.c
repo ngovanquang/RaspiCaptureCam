90a6650b147e29d401970e79fd4922b62d5875f5
ngovanquang <banbanhbeo@outlook.com>
2022-06-24 15:36:07 +0700

    update server

--- server/server.c
+++ server/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,14 +11,14 @@
 
 int socket_init(int *pSocket)
 {
+    int n;
     int connSockFd, sockFd;
     sockFd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("[socket]\t\treturned %d: %s\n", sockFd, strerror(errno));
     if(sockFd == -1)
     {
-        perror("socket()");
         return SV_FAILED;
     }
-    printf("socket create success!\n");
 
     struct sockaddr_in saddr, caddr;
     saddr.sin_family        = AF_INET;
@@ -25,28 +26,21 @@ int socket_init(int *pSocket)
     saddr.sin_addr.s_addr   = INADDR_ANY;
 
     //bind
-    if(bind(sockFd, (struct sockadd*)&saddr, sizeof(struct sockaddr)) == -1)
-    {
-        perror("bind()");
-        return SV_FAILED;
-    }
-    printf("binding...\n");
+    n = bind(sockFd, (struct sockaddr*)&saddr, sizeof(struct sockaddr));
+    printf("[bind]\t\treturned %d: %s\n", n, strerror(errno));
+    if (n == -1) return SV_FAILED;
+
     //listen
-    if(listen(sockFd, LISTEN_BACKLOG) == -1)
-    {
-        perror("listen()");
-        return SV_FAILED;
-    }
-    printf("listening...\n");
+    n = listen(sockFd, LISTEN_BACKLOG);
+    printf("[listen]\t\treturned %d: %s\n", n, strerror(errno));
+    if(n == -1) return SV_FAILED;
+
     //accept
     socklen_t clen = sizeof(caddr);
     connSockFd = accept(sockFd, (struct sockaddr*)&caddr, &clen);
-    if(connSockFd == -1)
-    {
-        perror("accept()");
-        return SV_FAILED;
-    }
-    printf("accepted...\n");
+    printf("[accept]\t\treturned %d: %s\n", connSockFd, strerror(errno));
+    if(connSockFd == -1) return SV_FAILED;
+
     *pSocket = connSockFd;
 
    return SV_SUCCESS; 
@@ -54,40 +48,52 @@ int socket_init(int *pSocket)
 
 int socket_deinit(int *pSocket)
 {
-    if(close(*pSocket) != 0)
-    {
-        perror("close()");
-        return SV_FAILED;
-    }
+    int n;
+    n = close(*pSocket);
+    printf("[close]\t\treturned %d: %s\n", n, strerror(errno));
+    if (n != 0) return SV_FAILED;
     return SV_SUCCESS;
 }
 
 int main(int argc, char* argv[])
 {
+    int n;
     int socket;
     char* buffer;
     buffer = (char*)calloc(BUFFER_SIZE, 1);
 
+    socket_init(&socket);
 
-    if(SV_FAILED == socket_init(&socket))
+    // strcpy(buffer, "OK 200 MOTION 7"); 
+    // write(socket, buffer, BUFFER_SIZE);
+
+    // communication
+    // step 1: recv request from client
+    while (1)
     {
-        printf("socket_init() failed\n");
+        memset(buffer, 0, BUFFER_SIZE);
+        n = read(socket, buffer, strlen(REQ_CMD));
+        printf("read returned %d , [data: %s]: %s\n", n, buffer, strerror(errno));
+        n = strcmp(buffer, REQ_CMD);
+        printf("strcmp returned %d: %s\n", n, strerror(errno));
+        if (n >= 0) break;
     }
-    printf("socket_init() successfull!\n");
 
-    strcpy(buffer, "OK 200 MOTION 7"); 
-    write(socket, buffer, BUFFER_SIZE);
+    // step 2: send ack to client
+    memset(buffer, 0, BUFFER_SIZE);
+    sprintf(buffer, "%s %d", RESP_CMD, RESP_CODE_OK);
+    write(socket, buffer, strlen(buffer));
+    printf("write returned %d , [data: %s]: %s\n", n, buffer, strerror(errno));
+    // step 3: recv img from client
 
+    // step 4: send ack to client
+    // step 5: send response to client
 
-
+    // end communication
 
     free(buffer);
 
-    if(SV_FAILED == socket_deinit(&socket))
-    {
-        printf("socket_deinit() failed\n");
-    }
-    printf("socket_deinit() successfull!\n");
+    socket_deinit(&socket);
     
     return 0;
 }
